a570e15d07eb329f2ab25d08797fe447ed0a450e
DJ-DVSA <david.james@DVSA.GOV.UK>
2025-06-13 12:42:47 +0100

    Added function to get the function and args from llm output

--- src/routing.py
+++ src/routing.py
@@ -5,8 +5,9 @@ import networkx as nx
 import osmnx as ox
 
 
-def get_continuing_road_path(G, current_road_name: str, next_road_name: str,
-                             current_node: str):
+def get_continuing_road_path(
+    G, current_road_name: str, next_road_name: str, current_node: str
+):
     """
     Finds the next node along a continuous path where the road changes name without an actual turn.
 
@@ -38,7 +39,7 @@ def get_continuing_road_path(G, current_road_name: str, next_road_name: str,
 
     while True:
         node = path[-1]
-        visited.add(node)    
+        visited.add(node)
         successors = [n for n in G.successors(node) if n not in visited]
         if not successors:
             raise ValueError("No further successors from current node")
@@ -47,24 +48,25 @@ def get_continuing_road_path(G, current_road_name: str, next_road_name: str,
         for succ in successors:
             edge_data = G.get_edge_data(node, succ)
             for _, data in edge_data.items():
-                road_name = data.get('name')
-                if (road_name == current_road_name or
-                        road_name == next_road_name):
+                road_name = data.get("name")
+                if road_name == current_road_name or road_name == next_road_name:
                     next_node = succ
                     break
             if next_node:
                 break
 
         if not next_node:
-            raise ValueError(f"Couldn't find continuation \
-                from {current_road_name} to {next_road_name}")
+            raise ValueError(
+                f"Couldn't find continuation \
+                from {current_road_name} to {next_road_name}"
+            )
 
         path.append(next_node)
 
         # Check if we've arrived on the next road
         edge_data = G.get_edge_data(node, next_node)
         for _, data in edge_data.items():
-            if data.get('name') == next_road_name:
+            if data.get("name") == next_road_name:
                 return path
 
 
@@ -84,7 +86,7 @@ def get_nodes_on_road(G, road_name: str) -> set:
     nodes = set()
     for u, v, data in G.edges(data=True):
         # u is the start node, v is the edge node, data is edge data
-        if data.get('name') == road_name:
+        if data.get("name") == road_name:
             nodes.add(u)
             nodes.add(v)
     return nodes
@@ -102,10 +104,10 @@ def turn_angle(current_node: dict, shared_node: dict, neighbour_node: dict):
     Returns:
         _type_: _description_
     """
-    x1 = shared_node['x'] - current_node['x']
-    y1 = shared_node['y'] - current_node['y']
-    x2 = neighbour_node['x'] - shared_node['x']
-    y2 = neighbour_node['y'] - shared_node['y']
+    x1 = shared_node["x"] - current_node["x"]
+    y1 = shared_node["y"] - current_node["y"]
+    x2 = neighbour_node["x"] - shared_node["x"]
+    y2 = neighbour_node["y"] - shared_node["y"]
 
     # print(f'x1: {x1}, y1: {y1}' )
     # print(f'x2: {x2}, y2: {y2}' )
@@ -119,9 +121,11 @@ def turn_angle(current_node: dict, shared_node: dict, neighbour_node: dict):
     return angle_deg + 180
 
 
-def get_turn_path(G, current_road_name: str, next_road_name: str, current_node: str, direction: str):
+def get_turn_path(
+    G, current_road_name: str, next_road_name: str, current_node: str, direction: str
+):
     """
-    Determines the OSMNX nodes in the path from the users current node on their current road to the node after the 
+    Determines the OSMNX nodes in the path from the users current node on their current road to the node after the
     junction that the user is turning onto
 
     Args:
@@ -133,7 +137,7 @@ def get_turn_path(G, current_road_name: str, next_road_name: str, current_node: 
 
     Returns:
         list: list of the nodes IDs from the current node to the first node on the next road after the junction.
-        
+
     Example:
         >>> get_turn_path(G, "High Street", "Elm Road", 123456, left)
         [123456, 123789, 23374, 1505050]
@@ -142,8 +146,10 @@ def get_turn_path(G, current_road_name: str, next_road_name: str, current_node: 
     curr_road_nodes = get_nodes_on_road(G, current_road_name)
     # 2) Get the nodes on the next road
     next_road_nodes = get_nodes_on_road(G, next_road_name)
-    # 3) Find the nodes common to both to get junction nodes 
-    junc_node = list(curr_road_nodes.intersection(next_road_nodes))  # get nodes in both roads
+    # 3) Find the nodes common to both to get junction nodes
+    junc_node = list(
+        curr_road_nodes.intersection(next_road_nodes)
+    )  # get nodes in both roads
     if len(junc_node) == 1:
         junc_node = list(curr_road_nodes.intersection(next_road_nodes))[0]
     elif len(junc_node) > 1:
@@ -151,10 +157,9 @@ def get_turn_path(G, current_road_name: str, next_road_name: str, current_node: 
         next_node_not_shared = next_road_nodes.difference(set(junc_node))
         # check for nodes on next road not in current
         junc_node = {
-            node for node in junc_node
-            if len({
-                n for n in G.neighbors(node)
-                }.intersection(next_node_not_shared))
+            node
+            for node in junc_node
+            if len({n for n in G.neighbors(node)}.intersection(next_node_not_shared))
         }
 
         # Check if there is one node left, otherwise use networkx to get shortest distance
@@ -162,51 +167,52 @@ def get_turn_path(G, current_road_name: str, next_road_name: str, current_node: 
             junc_node = list(junc_node)[0]
         else:
             junc_node = min(
-                junc_node, key=lambda node:
-                nx.shortest_path_length(G, current_node, node, weight="length")
+                junc_node,
+                key=lambda node: nx.shortest_path_length(
+                    G, current_node, node, weight="length"
+                ),
             )
 
     else:
         raise ValueError("No shared nodes between the two roads!")
     # 4) Get neighbours of common node
-    junc_node_neighbours = [neighbour for neighbour in G.neighbors(junc_node) 
-                            if (neighbour in next_road_nodes
-                            and neighbour not in curr_road_nodes)]
+    junc_node_neighbours = [
+        neighbour
+        for neighbour in G.neighbors(junc_node)
+        if (neighbour in next_road_nodes and neighbour not in curr_road_nodes)
+    ]
     # 5) Get angles of the turns
     neigbour_angles = [
-        turn_angle(
-            G.nodes[current_node], G.nodes[junc_node],
-            G.nodes[x]
-        )
+        turn_angle(G.nodes[current_node], G.nodes[junc_node], G.nodes[x])
         for x in junc_node_neighbours
-                       ]
+    ]
     # 7) Select Neighbour node dependent on turn direction
-    if direction == 'left':
-        final_node = junc_node_neighbours[
-            neigbour_angles.index(min(neigbour_angles))
-            ]
+    if direction == "left":
+        final_node = junc_node_neighbours[neigbour_angles.index(min(neigbour_angles))]
     elif direction == "right":
-        final_node = junc_node_neighbours[
-            neigbour_angles.index(max(neigbour_angles))
-            ]
+        final_node = junc_node_neighbours[neigbour_angles.index(max(neigbour_angles))]
     elif direction == "straight":
-        junc_node_neighbours[min(enumerate([
-            abs(180 - (n)) for n in neigbour_angles
-            ]), key=lambda x: x[1])[0]]
+        junc_node_neighbours[
+            min(
+                enumerate([abs(180 - (n)) for n in neigbour_angles]), key=lambda x: x[1]
+            )[0]
+        ]
     else:
         pass  # may need to add this??
     # 7) Get the route from current node to final node
     current_node_to_junction_pth = nx.shortest_path(
-        G, source=current_node, target=junc_node)
+        G, source=current_node, target=junc_node
+    )
     junction_to_turn_pth = nx.shortest_path(G, junc_node, final_node)
 
     return current_node_to_junction_pth[:-1] + junction_to_turn_pth
 
 
-def get_roundabout_path(G, current_road_name: str, next_road_name: str,
-                        current_node: str, exit: int):
+def get_roundabout_path(
+    G, current_road_name: str, next_road_name: str, current_node: str, exit: int
+):
     """
-    Determines the OSMNX nodes in the path from the users current node on their current road to the node after a 
+    Determines the OSMNX nodes in the path from the users current node on their current road to the node after a
     a roundabout exit that the user is turning onto
 
     This function navigates the road network graph starting from the current node,
@@ -241,7 +247,7 @@ def get_roundabout_path(G, current_road_name: str, next_road_name: str,
             # get the neighbours of the current road nodes
             for _, edge_data in G.get_edge_data(node, neighbour).items():
                 # get the edges of the current road nodes
-                if edge_data.get('junction') == 'roundabout':
+                if edge_data.get("junction") == "roundabout":
                     # check if any of the edges are roundabouts
                     potential_roundabout_current_road_nodes.add(node)
                     # append these edges to a list
@@ -250,7 +256,7 @@ def get_roundabout_path(G, current_road_name: str, next_road_name: str,
 
     # Look at the connected nodes to find the roundabout nodes
     for node in potential_roundabout_current_road_nodes:
-        queue = [node] # intiate a queue
+        queue = [node]  # intiate a queue
         visited = set([node])
         roundabout_nodes = [node]
 
@@ -261,7 +267,7 @@ def get_roundabout_path(G, current_road_name: str, next_road_name: str,
                     edge_ra_datas = G.get_edge_data(current_ra_node, nd_ra)
                     if edge_ra_datas:
                         for _, edge_ra_data in edge_ra_datas.items():
-                            if edge_ra_data.get('junction') == 'roundabout':
+                            if edge_ra_data.get("junction") == "roundabout":
                                 queue.append(nd_ra)
                                 visited.add(nd_ra)
                                 roundabout_nodes.append(nd_ra)
@@ -287,32 +293,34 @@ def get_roundabout_path(G, current_road_name: str, next_road_name: str,
                 if rb_nd_neighbour not in ra:
                     rd_nb_edges = G.get_edge_data(rb_nd, rb_nd_neighbour)
                     for _, data in rd_nb_edges.items():
-                        roundabout_exit_roads[rb_nd] = data.get('name')
+                        roundabout_exit_roads[rb_nd] = data.get("name")
 
             roundabout_exit_roads_dict[ra_key] = roundabout_exit_roads
 
     # Remove roundabouts that do not have any exits onto the road
     roundabout_exit_roads_dict = {
-        k: v for k, v in roundabout_exit_roads_dict.items()
+        k: v
+        for k, v in roundabout_exit_roads_dict.items()
         if next_road_name in v.values()
     }
 
     # check that there is only one roundabout still left after filtering
     if len(roundabout_exit_roads_dict) > 1:
-        raise ValueError("Multiple roundabouts after filtering")    
+        raise ValueError("Multiple roundabouts after filtering")
 
     if len(roundabout_exit_roads_dict) == 0:
         raise ValueError("No roundabouts found")
 
     # Get the most likely entrance node using shortest distance (this will be the path to the roundabout)
     roundabout_entrance_curr_node_nodes = {
-        k: ox.shortest_path(G, current_node, k,  weight='length')
+        k: ox.shortest_path(G, current_node, k, weight="length")
         for k, v in next(iter(roundabout_exit_roads_dict.values())).items()
-        if v in current_road_name}
+        if v in current_road_name
+    }
 
     roundabout_entrance_node, roundabout_entrance_path = min(
-        roundabout_entrance_curr_node_nodes.items(),
-        key=lambda item: len(item[1]))
+        roundabout_entrance_curr_node_nodes.items(), key=lambda item: len(item[1])
+    )
 
     # Get the exit node from the exit number
     roundabout_nodes = list(next(iter(roundabout_exit_roads_dict.values())).keys())
@@ -327,26 +335,81 @@ def get_roundabout_path(G, current_road_name: str, next_road_name: str,
 
     # Get route between roundabout entrance and exit nodes
     roundabout_entrance_exit_path = ox.shortest_path(
-        G, roundabout_entrance_node, roundabout_exit_node,
-        weight='length')
+        G, roundabout_entrance_node, roundabout_exit_node, weight="length"
+    )
 
     # Get the final node on the road
     final_node = [
-        nd for nd in G.successors(roundabout_exit_node)
-        if nd not in roundabout_nodes
-        ]
+        nd for nd in G.successors(roundabout_exit_node) if nd not in roundabout_nodes
+    ]
 
     # check if there aren't multiple routes off the exit node
     if len(final_node) != 1:
-        raise ValueError(f"Multiple nodes off exit node found from node: {
-            roundabout_exit_node}")
+        raise ValueError(
+            f"Multiple nodes off exit node found from node: {roundabout_exit_node}"
+        )
 
     final_path = roundabout_entrance_path[:-1]
-    + roundabout_entrance_exit_path + final_node
+    +roundabout_entrance_exit_path + final_node
 
     # check if there are any nodes on the current road on roundabouts
     if not potential_roundabout_current_road_nodes:
-        raise ValueError(f"Could not find any nodes connected \
-            to roundabouts on the current road: {current_road_name}")
+        raise ValueError(
+            f"Could not find any nodes connected \
+            to roundabouts on the current road: {current_road_name}"
+        )
 
     return final_path
+
+
+tool_dict = {
+    "get_continuing_road_path": get_continuing_road_path,
+    "get_turn_path": get_turn_path,
+    "get_roundabout_path": get_roundabout_path,
+}
+
+
+def run_tool(response, tool_dict: dict):
+    """_summary_
+
+    Args:
+        response (_type_): _description_
+        tool_dict (dict): _description_
+    """
+    tool = response.message.tool_calls[0]
+    function_to_call = tool_dict.get(tool.function.name)
+    if function_to_call:
+        print("Function output:", function_to_call(**tool.function.arguments))
+        
+        return function_to_call(**tool.function.arguments)
+    else:
+        print("Function not found:", tool.function.name)
+
+
+if __name__ == "__main__":
+    import os
+    import sys
+
+    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+    from src.llm_agent import INITIAL_PROMPT, run_llm
+
+    print("Hello")
+
+    # G_tolworth = ox.graph_from_point((51.3829463, -0.2933327), dist=5000, network_type='drive')
+    # print(get_nodes_on_road(G_tolworth, 'Ewell Road'))
+
+    node1_2_input = {
+        "current_node": "23780711",
+        "current_road": "Douglas Road",
+        "next_road": "Ewell Road",
+        "instruction": "Take the second left",
+    }
+
+    llm_response = run_llm(
+        input=node1_2_input,
+        prompt=INITIAL_PROMPT,
+        tools=[get_continuing_road_path, get_turn_path, get_roundabout_path],
+    )
+
+    func_out = run_tool(llm_response, tool_dict)
